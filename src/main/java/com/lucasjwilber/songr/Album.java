f7f4e296429bdfddf28bf824647f2a8f857595af
Lucas Wilber <lucasjwilber@gmail.com>
2020-01-22 10:08:43 -0800

    added psql dependencies, refactored Album class for use with repo

--- src/main/java/com/lucasjwilber/songr/Album.java
+++ src/main/java/com/lucasjwilber/songr/Album.java
@@ -1,13 +1,27 @@
 package com.lucasjwilber.songr;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Album {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    long id;
+
     public String title;
     public String artist;
     public int songCount;
     public int length;
     public String imageURL;
 
+    //default constructor for JPA
+    public Album() {};
+
     public Album(String title, String artist, int songCount, int length, String imageURL) {
+
         this.title = title;
         this.artist = artist;
         this.songCount = songCount;
--- src/main/java/com/lucasjwilber/songr/SongrController.java
+++ src/main/java/com/lucasjwilber/songr/SongrController.java
@@ -1,13 +1,20 @@
 package com.lucasjwilber.songr;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class SongrController {
+
+    @Autowired
+    SongrRepository repo;
+
 
     @GetMapping("/")
     public String splashPage() {
@@ -37,5 +44,12 @@ public class SongrController {
         m.addAttribute("albums", albums);
 
         return "albums";
+    }
+
+    @PostMapping("/albums")
+    public RedirectView addAlbum() {
+        System.out.println("addAlbum() entered");
+        repo.save(new Album());
+        return new RedirectView("/albums");
     }
 }
